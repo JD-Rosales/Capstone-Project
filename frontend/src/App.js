28e15b7c219b44fbbef76f037fac9e9753d7febe
John Paul Duavis <paulduavis09@gmail.com>
2022-09-24 18:32:04 +0800

    Updated App js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
-import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css'
 import { Routes, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
 import Landing from './pages/Landing/Landing';
 import Home from './pages/Home/Home';
 import Play from "./pages/Play/Play";
@@ -11,11 +10,10 @@ import SpellHandSign from "./pages/SpellHandSign/SpellHandSign";
 import FourPicOneWord from "./pages/FourPicOneWord/FourPicOneWord";
 import Learn from "./pages/Learn/Learn";
 import Login from "./pages/Login/Login";
-import TeacherSignUp from "./pages/SignUp/TeacherSignUp";
-import StudentSignUp from "./pages/SignUp/StudentSignUp";
-import GeneralUserSignUp from "./pages/SignUp/GeneralUserSignUp";
+import SignUp from "./pages/SignUp/SignUp";
 import WaitingApproval  from "./pages/WaitingApproval/WaitingApproval"
-
+import StudentDashboard from "./pages/StudentDashboard/StudentDashboard"
+import TeacherDashboard from "./pages/TeacherDashboard/TeacherDashboard";
 //Admin Pages
 import AdminLogin from './pages/Administrator/Login/Login'
 import Administrator from "./pages/Administrator/Administrator";
@@ -23,7 +21,13 @@ import AdminFingerSpell from "./pages/Administrator/AdminFingerSpell/AdminFinger
 import AdminSpellHandSign from "./pages/Administrator/AdminSpellHandSign/AdminSpellHandSign";
 import AdminFourPicOneWord from "./pages/Administrator/AdminFourPicOneWord/AdminFourPicOneWord"
 import AdminManageRequest from "./pages/Administrator/AdminManageRequest/AdminManageRequest";
+
+//Test Pages
+// import AppDrawer from "./pages/MUI/AppDrawer";
 function App() {
+  const { user } = useSelector(
+    (state) => state.auth
+  );
   return (
     <div className="App">
       <Routes>
@@ -36,23 +40,39 @@ function App() {
         <Route path="/spell-hand-sign" element={<SpellHandSign />} />
         <Route path="/4-pics-1-word" element={<FourPicOneWord />} />
         <Route path="/learn" element={<Learn />} />
-        <Route path="/user-login" element={<Login />} />
-        <Route path="/teacher-signup" element={<TeacherSignUp />} />
-        <Route path="/student-signup" element={<StudentSignUp />} />
-        <Route path= "/general-user-signup" element={<GeneralUserSignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/waiting-approval" element={<WaitingApproval />} />
+        <Route path="/student-dashboard" element={<StudentDashboard />} />
+        <Route path="/teacher-dashboard" element={<TeacherDashboard/>} />
 
-        <Route path="/login" element={<AdminLogin />} />
+        <Route path="/admin-login" element={<AdminLogin />} />
         <Route path="/administrator" element={<Administrator />} />
         <Route path="/manage-finger-spell" element={<AdminFingerSpell />} />
         <Route path="/manage-spell-hand-sign" element={<AdminSpellHandSign />} />
         <Route path="/manage-4-pic-1-word" element={<AdminFourPicOneWord />} />
         <Route path="/manage-request" element={<AdminManageRequest />} />
-      </Routes>
-      <ToastContainer
-        autoClose={1500}
-        pauseOnFocusLoss={false}
-      />
+
+        </Routes>
+
+
+        {/* { user ? 
+        (
+          <>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+          </>
+        ) :
+
+        (
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Routes>
+        )
+        } */}
     </div>
   );
 }
