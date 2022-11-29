767967ceb59cf7602c6000f5193eb7f8b7cc1d39
JD-Rosales <rosales.jake1999@gmail.com>
2022-11-29 20:41:22 +0800

    added middleware for suspended account

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,33 +1,38 @@
-const jwt = require('jsonwebtoken')
-const User = require('../models/userModel')
+const jwt = require("jsonwebtoken");
+const User = require("../models/userModel");
 
 const protected = async (req, res, next) => {
   try {
-    let token
-    
+    let token;
+
     // check if there is a token in request header
-    if(!req.headers.authorization) {
-      return res.status(401).json({message: "Unauthorized, no token"})
+    if (!req.headers.authorization) {
+      return res.status(401).json({ message: "Unauthorized, no token" });
     }
 
-    token = req.headers.authorization.split(' ')[1]
+    token = req.headers.authorization.split(" ")[1];
 
     // verify token
-    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY)
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
 
-    const user = await User.findById(decoded.id).select('-password')
+    const user = await User.findById(decoded.id).select("-password");
 
-    if(!user){
-      return res.status(401).json({message: "Unauthorized, invalid user"})
+    if (!user) {
+      return res.status(401).json({ message: "Unauthorized, invalid user" });
     }
-    
-    // attach user information into the header
-    req.user = user
-    next()
-    
-  } catch (error) {
-    return res.status(400).json({message: "Unauthorized, invalid credentials"})
-  }
-}
 
-module.exports = { protected }
+    if (!user.isActive) {
+      return res.status(403).json({ message: "Account Suspended" });
+    }
+
+    // attach user information into the header
+    req.user = user;
+    next();
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ message: "Unauthorized, invalid credentials" });
+  }
+};
+
+module.exports = { protected };
